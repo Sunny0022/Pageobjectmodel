46810bf8dbccc355127d26966a8d4981aca59dd1
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-05-26 19:04:03 +0530

    Create login_page_object.rb

---
+++ login_page_object.rb
@@ -0,0 +1,12 @@
+require 'page-object'
+class LoginPageObject
+  include PageObject
+
+  link(:loginlink, :xpath => '/html/body/nav/div[2]/ul/li[2]/a')
+  text_field(:username, :id => 'email')
+  text_field(:userpassword, :id => 'password')
+  radio_button(:radioclick, :id => 'remember')
+  button(:loginbutton, :xpath => '/html/body/div/div/div[1]/div/div/div[2]/form/div/div[2]/input')
+  link(:signout, :xpath => '/html/body/nav/div[2]/ul/li[4]/a')
+
+end
