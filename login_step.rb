8a526a548a27d797c7c231a772198fcae415a3c1
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-06-05 16:46:22 +0530

    Update login_step.rb

--- login_step.rb
+++ login_step.rb
@@ -1,4 +1,5 @@
 require_relative 'plogin_page'
+require 'test-unit'
 
 Given(/^i go to site page$/) do
   @user = PLogin.new(@driver)
@@ -18,12 +19,18 @@ And(/^i click login button$/) do
   @user.click_button
 end
 
+And(/^i verify the logout button$/) do
+  assert(@user.valid_user.displayed?)
+  sleep(10)
+end
+
 Then(/^user is signed in$/) do
-  @user.valid
+  @user.logout_user
   @user.close
 end
 
 Then(/^user is not signed in$/) do
-  @user.invalid
+  assert(@user.invalid_user.text.include?('Invalid credentials'), 'Please verify your email first')
+  sleep(10)
   @user.close
 end
